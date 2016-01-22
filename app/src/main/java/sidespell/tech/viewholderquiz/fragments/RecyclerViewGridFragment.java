f2a27d6a11268f72f7df6ad5f5ac07d525258195
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-01-22 23:10:41 +0800

    Implemented creating a RecyclerView with GridLayoutManager.

--- app/src/main/java/sidespell/tech/viewholderquiz/fragments/RecyclerViewGridFragment.java
+++ app/src/main/java/sidespell/tech/viewholderquiz/fragments/RecyclerViewGridFragment.java
@@ -3,19 +3,32 @@ package sidespell.tech.viewholderquiz.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import sidespell.tech.viewholderquiz.R;
+import sidespell.tech.viewholderquiz.adapters.RecyclerViewAdapter;
+import sidespell.tech.viewholderquiz.controllers.MoviesController;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RecyclerViewGridFragment extends Fragment {
 
+    private MoviesController mController;
+
     public static RecyclerViewGridFragment newInstance() {
         return new RecyclerViewGridFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mController = new MoviesController();
     }
 
     @Override
@@ -26,6 +39,26 @@ public class RecyclerViewGridFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        // TODO: Perform logic execution here..
+        // Find all the views
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerGridView);
+        TextView tvEmpty = (TextView) view.findViewById(android.R.id.empty);
+
+        // Setup the grid layout manager
+        GridLayoutManager glm = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(glm);
+        recyclerView.setHasFixedSize(true);
+
+        // Create the adapter
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(),
+                R.layout.recycler_grid_item, mController.getMovies());
+
+        // Set the adapter
+        recyclerView.setAdapter(adapter);
+
+        if (adapter.getItemCount() == 0) {
+            tvEmpty.setVisibility(View.VISIBLE);
+        } else {
+            tvEmpty.setVisibility(View.GONE);
+        }
     }
 }
